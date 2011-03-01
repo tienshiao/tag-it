f869fe5b787b59113b5448d4f570e27739f25485
Cloudmanic Labs, LLC <github@cloudmanic.com>
2011-02-28 17:10:43 -0800

    Added support for pre-defined tags. You just add the tags as <li>'s

--- js/tag-it.js
+++ js/tag-it.js
@@ -12,11 +12,32 @@
 		// add the tagit CSS class.
 		el.addClass("tagit");
 
+		// get any current tags. 
+		el.find('li').each(function (index) {
+			tags[index] = $(this).text();
+		});
+
 		// create the input field.
 		var html_input_field = "<li class=\"tagit-new\"><input class=\"tagit-input\" type=\"text\" /></li>\n";
 		el.html (html_input_field);
 
 		tag_input		= el.children(".tagit-new").children(".tagit-input");
+
+		// add support for tags already set at page load.
+		var i = 0;
+		for(i = 0; i < tags.length; i++)
+		{
+			tags[i] = tags[i].replace(/,+$/,"");
+			tags[i] = tags[i].trim();
+			
+			if (tags[i] != "") {
+				if (is_new (tags[i])) {
+					create_choice (tags[i]);
+				}
+				// Cleaning the input.
+				tag_input.val("");
+			}
+		}
 
 		$(this).click(function(e){
 			if (e.target.tagName == 'A') {
